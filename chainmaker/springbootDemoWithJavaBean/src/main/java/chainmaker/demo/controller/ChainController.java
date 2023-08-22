e6857cf3baecc36c8856be674352553bcddcac72
杨大智 <19231165@buaa.edu.cn>
2023-08-22 18:09:42 +0800

    这两版稍微有点问题 版本不太对

--- chainmaker/springbootDemoWithJavaBean/src/main/java/chainmaker/demo/controller/ChainController.java
+++
@@ -1,9 +0,0 @@
-package chainmaker.demo.controller;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-
-@CrossOrigin
-@Controller
-public class ChainController {
-}
